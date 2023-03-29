2e85ccde25d89c8a58ab31d788f67463905b8b9a
BrianRuszkowski <72171622+BrianRuszkowski@users.noreply.github.com>
2023-03-29 03:10:13 +0100

    Completed Logbook 1 exercises 1-7

---
+++ Logbooks CW1/wk1ex4.php
@@ -0,0 +1,19 @@
+<html>
+	<body>
+		<p>
+			<?php
+				echo gmdate("D");
+			?>
+		</p>
+		<p>
+			<?php
+				echo gmdate("M d Y");
+			?>
+		</p>
+		<p>
+			<?php
+				echo gmdate("z");
+			?>
+		</p>
+	</body>
+</html>
