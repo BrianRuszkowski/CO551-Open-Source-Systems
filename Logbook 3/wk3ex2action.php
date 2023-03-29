a247620e207feddbb634493fd77e643d948cbe77
BrianRuszkowski <72171622+BrianRuszkowski@users.noreply.github.com>
2023-03-29 04:46:51 +0100

    Completed Logbook 3 exercises 1-4

---
+++ Logbook 3/wk3ex2action.php
@@ -0,0 +1,19 @@
+<?php
+  if ($_POST["txtage"] < 21) 
+  {
+	  echo "You are under 21 years old<br/>";
+  }
+  else
+  {
+	  echo "You are  21 years old or over<br/>";
+  }
+
+  if($_POST["txtgender"] == 'Female')
+  {
+    echo "Your gender is Female";
+  }
+  else if ($_POST["txtgender"] == 'Male')
+  {
+    echo "Your gender is Male";
+  }
+?>
